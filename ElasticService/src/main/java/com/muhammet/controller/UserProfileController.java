2f4c1893529f6ab03fa82280f7a5d059b697a146
Muhammet Ali KAYA <info@beemeapp.com>
2023-04-24 12:22:55 +0300

    24.04.2023 gün sonu güncelleme

--- ElasticService/src/main/java/com/muhammet/controller/UserProfileController.java
+++ ElasticService/src/main/java/com/muhammet/controller/UserProfileController.java
@@ -4,6 +4,7 @@ import com.muhammet.dto.request.UserProfileDto;
 import com.muhammet.repository.entity.UserProfile;
 import com.muhammet.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,4 +28,9 @@ public class UserProfileController {
         return ResponseEntity.ok(userProfileService.findAll());
     }
 
+
+    @GetMapping("/findallpage")
+    public ResponseEntity<Page<UserProfile>> findAll(int currentPage,int size, String sortParameter,String sortDirection){
+        return ResponseEntity.ok(userProfileService.findAll(currentPage,size,sortParameter,sortDirection));
+    }
 }
--- ElasticService/src/main/java/com/muhammet/service/UserProfileService.java
+++ ElasticService/src/main/java/com/muhammet/service/UserProfileService.java
@@ -5,6 +5,10 @@ import com.muhammet.mapper.IUserProfileMapper;
 import com.muhammet.repository.IUserProfileRepository;
 import com.muhammet.repository.entity.UserProfile;
 import com.muhammet.utility.ServiceManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +22,32 @@ public class UserProfileService extends ServiceManager<UserProfile,String> {
     public void save(UserProfileDto dto){
         UserProfile user = IUserProfileMapper.INSTANCE.toUserProfile(dto);
         save(user);
+    }
+
+    /**
+     * Sayfalama işlemleri;
+     * Bir kayıt bilgisinde çok fazla datanını olması nedeniyle bilgilerin belli
+     * bir sayfalama yöntemi ile çağırılması.
+     * Hangi Sayfadasın? Page
+     * Bir Sayfada Kaç Kayıt Var? Size
+     * Gösterdiğin Kayıtları Sıralayacak mısın? Sort
+     */
+    public Page<UserProfile> findAll(int currentPage,int size, String sortParameter,String sortDirection){
+        /**
+         * Sort  -> Sıralama işlemlerini belirtir.
+         * Pageable -> Sayfalama işlemlerini belirtir.
+         */
+        Sort sort = null;
+        Pageable pageable = null;
+
+        if(sortParameter !=null)
+            sort = Sort.by(Sort.Direction.fromString(sortDirection==null ? "ASC" : sortDirection),sortParameter);
+
+        if(sort!=null){
+            pageable = PageRequest.of(currentPage,size == 0 ? 10 : size, sort);
+        }else{
+            pageable = PageRequest.of(currentPage, size == 0 ? 10 : size);
+        }
+        return repository.findAll(pageable);
     }
 }
